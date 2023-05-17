461f36ee7ab2457648f1bce2fc4f0f7316f2198e
TungOscar <a9825434@gmail.com>
2023-05-17 19:48:48 +0800

    water

---
+++ Web411018654/Controllers/waterController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web411018654.Controllers
+{
+    public class waterController : Controller
+    {
+        // GET: water
+        public ActionResult Index(String name,float?weight)
+        {
+            var result = "";
+            if (weight.HasValue)
+            {
+                result = (weight * 30) + "CC";
+            }
+            ViewBag.Name = name;
+            ViewBag.Result = result;
+            return View();
+        }
+    }
+}
